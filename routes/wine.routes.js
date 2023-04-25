8b98bd88b57448b4675c16cce1d43399fec1ac7f
Alexandra Strang <bouadloun1@gmail.com>
2023-04-25 17:03:28 +0200

    created wine route

--- routes/wine.routes.js
+++ routes/wine.routes.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Wine = require("../models/Wine.model")
+const Wine = require("../models/Wine.model");
 
-/* GET home page */
+/* GET wine page */
 
+router.get("/wines", (req, res, next) => {
+  // Get all wines from db
+  Wine.find()
+    .then((winesFromDB) => {
+      console.log(winesFromDB);
+      res.render("wine/wines", { wines: winesFromDB });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/wine/wines", (req, res, next) => {
+  console.log(req.body);
+  const { image_url, wine, winery, about, origin, rating } = req.body;
+
+  Wine.create({ image_url, wine, winery, about, origin, rating })
+    .then((createdWine) => {
+      console.log(createdWine);
+      res.redirect(`/wine/wines/${createdWine._id}`);
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
