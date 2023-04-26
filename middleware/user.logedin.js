ce33848a571c3ef21035f9a91b335406a3efa63d
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-04-26 12:08:42 +0200

    created profile route

---
+++ middleware/user.logedin.js
@@ -0,0 +1,11 @@
+function isLoggedIn(req, res, next) {
+	// Check if user is logged in
+	if (!req.session.user) {
+    return res.redirect("/user/login")
+	} 
+  
+  // User is logged in => Open requested page 
+  next()
+}
+
+module.exports = { isLoggedIn }
